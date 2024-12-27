26a5bbf89d06287e5b35cd483e652154225b570a
TahseenAdit <tahseen.adit@gmail.com>
2024-12-27 21:18:34 +0100

    added BSD 2-Clause license

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
